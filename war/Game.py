59ce584f986b6566921d8f810ffd88ed351bef34
Jesper Nilsson <70292202+CopycatPandaExpressChowMein@users.noreply.github.com>
2025-10-23 23:19:27 +0200

    Giga refactor

--- war/Game.py
+++ war/Game.py
@@ -1,3 +1,4 @@
+import datetime
 from Deck import Deck
 from Player import Player
 from Intelligence import Intelligence
@@ -35,81 +36,89 @@ class Game:
         for player in self.__players:
             self.__highscore.add_player(player.get_name())
 
-        print(f"{self.__player1.get_name()} vs {self.__player2.get_name()}, The Game has started!")
+        self.num_draws = 0 #Counter for the number of draws taken per game. Incremented each time cards are drawn.
+
         self.__active_game = True
+        print(f"{self.__player1.get_name()} vs {self.__player2.get_name()}, The Game has started!")
+        
 
     def get_active_game(self):
         """Returns a bool indicating whether or not a game is ongoing or not."""
         return self.__active_game
 
-
-    #TODO
     def cheat(self):
-        """allows you to cheat in the game"""
-        print("Place holder cheat")
+        """Allows you to cheat in the game"""
+        pass
 
-    #TODO
-    def draw_cards(self, war_pile=None):
-        # Initialize war pile to keep cards on table for war rounds
-        if war_pile is None:
-            war_pile = []
+    #TODO Graphics
+    def draw_cards(self):
 
-        hand0 = self.players[0].get_hand()
-        hand1 = self.players[1].get_hand()
+        player1_hand = self.__players[0].get_hand()
+        player2_hand = self.__players[1].get_hand()
+
+        player1_name = self.__players[0].get_name()
+        player2_name = self.__players[1].get_name()
 
         # Check if any player ran out of cards, game over condition
-        if len(hand0.getHand()) == 0:
-            print(f"{self.players[1].get_name()} wins the game! {self.players[0].get_name()} has no more cards.")
+        if len(player1_hand.getHand()) == 0:
+            #Player 2 wins, player 1 ran out of cards
+            print(f"{player2_name} wins the game! {player1_name} has no more cards.")
+            self.__highscore.add_statistics(player2_name, True, self.num_draws, datetime.date.today())
             return
-        if len(hand1.getHand()) == 0:
-            print(f"{self.players[0].get_name()} wins the game! {self.players[1].get_name()} has no more cards.")
+        elif len(player2_hand.getHand()) == 0:
+            #Player 1 wins, player 2 ran out of cards
+            print(f"{player1_name} wins the game! {player2_name} has no more cards.")
+            self.__highscore.add_statistics(player1_name, True, self.num_draws, datetime.date.today())
             return
 
         # Each player draws a card
-        card1 = hand0.drawcard()
-        card2 = hand1.drawcard()
+        player1_card = player1_hand.drawcard()
+        player2_card = player2_hand.drawcard()
+        self.num_draws += 1
 
-        print(f"{self.players[0].get_name()} played {card1}")
-        print(f"{self.players[1].get_name()} played {card2}")
-
-        # Put these cards on war pile
-        war_pile.extend([card1, card2])
 
         # Compare card values using correct get_value() method
-        if card1.get_value() > card2.get_value():
-            print(f"{self.players[0].get_name()} wins the round and takes {len(war_pile)} cards.")
-            # Fix: add each card using addCard instead of addcards
-            for c in war_pile:
-                hand0.addCard(c)
-            war_pile.clear()
-        elif card1.get_value() < card2.get_value():
-            print(f"{self.players[1].get_name()} wins the round and takes {len(war_pile)} cards.")
-            for c in war_pile:
-                hand1.addCard(c)
-            war_pile.clear()
+        if player1_card.get_value() > player2_card.get_value():
+            player2_active_cards = player2_hand.get_active_card()
+            print(f"{player1_name} wins the round and takes {len(player2_active_cards)} cards from {player2_name}.")
+            i, tmp = 0, len(player2_active_cards)
+            while i < tmp:
+                player1_hand.addCard(player2_hand.removeCard())
+                i += 1
+            player1_hand.return_cards()
+        elif player1_card.get_value() < player2_card.get_value():
+            player1_active_cards = player1_hand.get_active_card()
+            print(f"{player2_name} wins the round and takes {len(player1_active_cards)} cards from {player1_name}.")
+            i, tmp = 0, len(player1_active_cards)
+            while i < tmp:
+                player2_hand.addCard(player1_hand.removeCard())
+                i += 1
+            player2_hand.return_cards()
         else:
             print("War! Players place one card face down and one card face up.")
-
             # Check if players have enough cards for war
-            if len(hand0.getHand()) < 2:
-                print(f"{self.players[0].get_name()} cannot continue war - loses!")
-                for c in war_pile + hand0.getHand():
-                    hand1.addCard(c)
-                hand0.getHand().clear()
+            if len(player1_hand.getHand()) < 2:
+                print(f"{player1_name} cannot continue war - loses!")
+                for card in player1_hand.get_active_card():
+                    player2_hand.addCard(player1_hand.removeCard())
+                player2_hand.return_cards()
                 return
-            if len(hand1.getHand()) < 2:
-                print(f"{self.players[1].get_name()} cannot continue war - loses!")
-                for c in war_pile + hand1.getHand():
-                    hand0.addCard(c)
-                hand1.getHand().clear()
+            if len(player2_hand.getHand()) < 2:
+                print(f"{player2_name} cannot continue war - loses!")
+                for card in player2_hand.get_active_card():
+                    player1_hand.addCard(player2_hand.removeCard())
+                player1_hand.return_cards()
                 return
 
             # Each player places one card face down (on war pile)
-            war_pile.append(hand0.drawcard())
-            war_pile.append(hand1.drawcard())
+            player1_hand.drawcard()
+            player2_hand.drawcard()
+            self.num_draws += 1
+
 
             # Recursively call draw_cards to determine who wins the war
-            self.draw_cards(war_pile)
+            self.draw_cards()
+
 
     def name_change(self, current_name, new_name):
         """ 
